1d585b2fd87ae46fd20ed82a852f206728299ffa
Mima Farley-Rose <mimafarlyrose1997@gmail.com>
2020-08-10 17:34:59 +0100

    add alt attribute

--- src/components/sections/work-section.jsx
+++ src/components/sections/work-section.jsx
@@ -5,10 +5,10 @@ export const WorkSection = () => {
 
     return (
         <div className={'work-section'}>
-            <img src={"https://myventurepad.com/wp-content/uploads/2017/05/your-website.jpg"} className={"work-examples"}/>
-            <img src={"https://myventurepad.com/wp-content/uploads/2017/05/your-website.jpg"} className={"work-examples"}/>
-            <img src={"https://myventurepad.com/wp-content/uploads/2017/05/your-website.jpg"} className={"work-examples"}/>
-            <img src={"https://myventurepad.com/wp-content/uploads/2017/05/your-website.jpg"} className={"work-examples"}/>
+            <img src={"https://myventurepad.com/wp-content/uploads/2017/05/your-website.jpg"} alt={"placeholder"} className={"work-examples"}/>
+            <img src={"https://myventurepad.com/wp-content/uploads/2017/05/your-website.jpg"} alt={"placeholder"} className={"work-examples"}/>
+            <img src={"https://myventurepad.com/wp-content/uploads/2017/05/your-website.jpg"} alt={"placeholder"} className={"work-examples"}/>
+            <img src={"https://myventurepad.com/wp-content/uploads/2017/05/your-website.jpg"} alt={"placeholder"} className={"work-examples"}/>
         </div>
     );
 };
