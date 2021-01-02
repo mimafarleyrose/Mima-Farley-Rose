346554cd140730526ed146d711d648f043751d96
Jemima Farley-Rose <Mima@Laptop-for-Beans.local>
2021-01-02 17:07:15 +0000

    update cv

--- src/components/sections/cv-button.jsx
+++ src/components/sections/cv-button.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import './sections.css'
-import CV from '../cv.98694ab1.pdf'
-import { DownloadOutlined } from '@ant-design/icons';
-
+import "./sections.css";
+import CV from "../jemima_cv.pdf";
+import { DownloadOutlined } from "@ant-design/icons";
 
 export const CvButton = (props) => {
-
-    return (
-        <div className = "cv-section">
-            <a href = {CV} target = "_blank" rel="noopener noreferrer">
-                <button
-                    type="primary"
-                    shape="circle"
-                    icon={<DownloadOutlined />}
-                    className={`download-cv__${props.theme?'light':'dark'}`} >
-                    CV
-                </button>
-            </a>
-        </div>    );
+  return (
+    <div className="cv-section">
+      <a href={CV} target="_blank" rel="noopener noreferrer">
+        <button
+          type="primary"
+          shape="circle"
+          icon={<DownloadOutlined />}
+          className={`download-cv__${props.theme ? "light" : "dark"}`}
+        >
+          CV
+        </button>
+      </a>
+    </div>
+  );
 };
